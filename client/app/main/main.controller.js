07141e678794be3832fe1edd696ae3294f425ec8
Rumman Ahmed <ahmed.rumman@gmail.com>
2015-02-04 22:53:03 +1100

    data capture ready

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -28,8 +28,9 @@ angular.module('serveMeApp')
       $http.delete('/api/things/' + thing._id);
      };
 
-    $scope.formData = {} ;
+   $scope.formData = {} ;
 
+   $scope.lastItem_id = '';
 
    $http.get('api/datas/').success(function(getData){
       $scope.dataRepo = getData;
@@ -79,7 +80,7 @@ angular.module('serveMeApp')
   $scope.isWindow = true;
 
   // TRACE LOG Functions   
-  $scope.traceLaunch = function(itemName){  
+  $scope.traceLaunch = function (itemName){  
     var launchCount =0;
       $http.get('/api/tracelogs/items/').success(function(getLastEntry){
           console.log(getLastEntry.launchCount)
@@ -96,57 +97,38 @@ angular.module('serveMeApp')
        launchCount : launchCount+1,
        launchTime  : new Date()
       }).success(function(response){
-        console.log("response",response);
+        console.log("response",response._id);
+        $scope.lastItem_id = response._id;
       });
      console.log("launch data recorded " + launchCount);
     },300);
     $scope.isLaunch  = true;
     $scope.isWindow  = false;
+    $("#clickStatus").show();
    };
-  $scope.traceWindow = function(itemName){  
-    var launchCount =0;
-      $http.get('/api/tracelogs/items/').success(function(getLastEntry){
-          console.log(getLastEntry.launchCount)
-          launchCount = getLastEntry.launchCount;
-          if(launchCount == undefined){
-            launchCount = 0;
-          }
-        });
-
-    // add entry to db
-    setTimeout(function(){
-     $http.post("/api/tracelogs/",{
-       itemName : itemName,
-       launchCount : launchCount+1,
-       launchTime  : new Date()
+  $scope.traceWindow = function (itemName){  
+    console.log("now last id is : " , $scope.lastItem_id);
+    var targetId =  $scope.lastItem_id ;
+    // update window open time for tagetId to db
+     $http.put("/api/tracelogs/"+targetId,{
+       midTime: new Date() 
       })
-     console.log("launch data recorded " + launchCount);
-    },300);
     $scope.isWindow  = true;
     $scope.isReady  = false;
    }; 
-   $scope.traceReady = function(itemName){  
-    var launchCount =0;
-      $http.get('/api/tracelogs/items/').success(function(getLastEntry){
-          console.log(getLastEntry.launchCount)
-          launchCount = getLastEntry.launchCount;
-          if(launchCount == undefined){
-            launchCount = 0;
-          }
-        });
-
-    // add entry to db
-    setTimeout(function(){
-     $http.post("/api/tracelogs/",{
-       itemName : itemName,
-       launchCount : launchCount+1,
-       launchTime  : new Date()
+  $scope.traceReady  = function (itemName){  
+    console.log("now last id is : " , $scope.lastItem_id);
+    var targetId =  $scope.lastItem_id ;
+    // update window open time for tagetId to db
+     $http.put("/api/tracelogs/"+targetId,{
+       endTime: new Date() 
       })
-     console.log("launch data recorded " + launchCount);
-    },300);
+
     $scope.isReady  = true;
     $scope.isLaunch  = false;
+    $("#clickStatus").delay(2500).fadeOut("slow");
    };  
+
   // ########## Event Controls with socketio #########
   $scope.$on('$destroy', function () {
       socket.unsyncUpdates('thing');
